d86e9bb3c1b52e5fa636af4646184b124fdc90b9
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-08-25 14:42:00 +0200

    Added bootstrap styling to Create page

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,22 +1,25 @@
-
 @extends('layouts.app')
 
 @section('content')
-    <h1>Add New Task</h1>
-    @if($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form method="POST" action="{{ url('/tasks') }}">
-        @csrf
-        <label for="title">Task Title:</label>
-        <input type="text" name="title" id="title" value="{{ old('title') }}">
-        <button type="submit">Add Task</button>
-    </form>
-    <a href="{{ url('/') }}">Back to Task List</a>
+    <div class="container mt-5">
+        <h1>Add New Task</h1>
+        @if($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form method="POST" action="{{ url('/tasks') }}">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Task Title:</label>
+                <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}">
+            </div>
+            <button type="submit" class="btn btn-primary">Add Task</button>
+        </form>
+        <a href="{{ url('/') }}" class="btn btn-secondary mt-3">Back to Task List</a>
+    </div>
 @endsection
