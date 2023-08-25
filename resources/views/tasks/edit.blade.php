4403e90ae352089ef27c61b6f6eec58e35a88308
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-08-25 14:56:31 +0200

    Missing validation messages in Edit view added

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,6 +3,15 @@
 @section('content')
     <div class="container mt-5">
         <h1>Edit Task</h1>
+        @if($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <form action="{{ route('tasks.update', $index) }}" method="post">
             @method('PATCH')
             @csrf
