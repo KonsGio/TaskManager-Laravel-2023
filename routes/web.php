1612432e82bc4a0bb678603aa98110942c7d4f45
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-08-25 14:36:01 +0200

    Basic routing for the pages created

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'index']);
+Route::get('/tasks/create', [TaskController::class, 'create']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::patch('/tasks/{task}', [TaskController::class, 'complete']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
