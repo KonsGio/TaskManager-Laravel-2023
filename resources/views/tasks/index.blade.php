d7ddd34326a502575628822efbb4482fc597bcbd
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-08-25 14:50:49 +0200

    Task new background color when checked and button options rearranged

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,13 +5,13 @@
         <h1>Task List</h1>
         <ul class="list-group">
             @foreach($tasks as $index => $task)
-                <li class="list-group-item d-flex justify-content-between align-items-center">
+            <li class="list-group-item d-flex justify-content-between align-items-center {{ $task['completed'] ? 'bg-light' : '' }}">
                     <div class="d-flex align-items-center">
                         @if (!$task['completed'])
                             <form action="{{ route('tasks.complete', $index) }}" method="post">
                                 @method('PATCH')
                                 @csrf
-                                <button type="submit" class="btn btn-success btn-sm">
+                                <button type="submit" class="btn btn-success btn-md">
                                     <input type="hidden" name="completed" value="1">
                                 </button>
                             </form>
@@ -23,10 +23,15 @@
                             <form action="{{ route('tasks.restore', $index) }}" method="post">
                                 @method('PATCH')
                                 @csrf
-                                <button type="submit" class="btn btn-dark btn-sm">
+                                <button type="submit" class="btn btn-secondary btn-sm">
                                     <input type="hidden" name="completed" value="0">
                                     Restore
                                 </button>
+                            </form>
+                            <form action="{{ route('tasks.destroy', $index) }}" method="post" class="ms-1">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
                             </form>
                         @else
                             <a href="{{ route('tasks.edit', $index) }}" class="btn btn-warning btn-sm">Edit</a>
