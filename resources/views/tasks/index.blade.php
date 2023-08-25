ad20d7f9ff70850137f6d03d8699af62cb3bad09
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-08-25 14:48:26 +0200

    Basic completed button and restore button added

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,21 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Task List</h1>
-    <ul>
-        @foreach($tasks as $index => $task)
-            <li>
-                {{ $task['title'] }}
-                @if (!$task['completed'])
-                    <a href="{{ url("/tasks/$index/complete") }}">Mark as Completed</a>
-                @endif
-                <form action="{{ url("/tasks/$index") }}" method="post">
-                    @method('DELETE')
-                    @csrf
-                    <button type="submit">Delete</button>
-                </form>
-            </li>
-        @endforeach
-    </ul>
-    <a href="{{ url('/tasks/create') }}">Add New Task</a>
+    <div class="container mt-5">
+        <h1>Task List</h1>
+        <ul class="list-group">
+            @foreach($tasks as $index => $task)
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <div class="d-flex align-items-center">
+                        @if (!$task['completed'])
+                            <form action="{{ route('tasks.complete', $index) }}" method="post">
+                                @method('PATCH')
+                                @csrf
+                                <button type="submit" class="btn btn-success btn-sm">
+                                    <input type="hidden" name="completed" value="1">
+                                </button>
+                            </form>
+                        @endif
+                        <span class="ms-2 {{ $task['completed'] ? 'text-decoration-line-through' : '' }}">{{ $task['title'] }}</span>
+                    </div>
+                    <div class="btn-group" role="group">
+                        @if ($task['completed'])
+                            <form action="{{ route('tasks.restore', $index) }}" method="post">
+                                @method('PATCH')
+                                @csrf
+                                <button type="submit" class="btn btn-dark btn-sm">
+                                    <input type="hidden" name="completed" value="0">
+                                    Restore
+                                </button>
+                            </form>
+                        @else
+                            <a href="{{ route('tasks.edit', $index) }}" class="btn btn-warning btn-sm">Edit</a>
+                            <form action="{{ route('tasks.destroy', $index) }}" method="post" class="ms-1">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            </form>
+                        @endif
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+        <a href="{{ url('/tasks/create') }}" class="btn btn-primary mt-3">Add New Task</a>
+    </div>
 @endsection
